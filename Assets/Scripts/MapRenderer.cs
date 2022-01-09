464d4a8b10dcfe7a0aa988baf4a13e5955181f8d
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2022-01-09 13:19:55 -0500

    Added auto-pipe generator

--- Assets/Scripts/MapRenderer.cs
+++ Assets/Scripts/MapRenderer.cs
@@ -2,40 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-class Pipe
-{
-    Transform _Head;
-    Transform _Body;
-    public Pipe(Transform Head, Transform Body)
-    {
-        _Head = Head;
-        _Body = Body;
-    }
-    public void Move(float distance)
-    {
-        Vector3 newPos = Vector3.left * distance * Time.deltaTime;
-        _Head.position += newPos;
-        _Body.position += newPos;
-    }
-    public float getXPos { get => _Head.position.x; }
-    public void Destory()
-    {
-        Object.Destroy(_Head.gameObject);
-        Object.Destroy(_Body.gameObject);
-        
-    }
-}
+
 
 public class MapRenderer : MonoBehaviour
 {
     private const float HeadHeight = 5f;
     [SerializeField]
     private Camera Camera;
+    private float Pipe_Add_Pos = 130f;
+    private float SpawnHeightLimit = 10f;
+    // Pipe Runtime Data
     private List<Pipe> PipeComponents = new List<Pipe>();
+    private float pipeSpawnTimer;
+    private int SpawnedPipes;
     private void Start()
     {
-        newPipe(45, 50f);
-        //newPipe(30, 0f);
+        
     }
     private void Update()
     {
@@ -43,21 +25,44 @@ public class MapRenderer : MonoBehaviour
         for (int i=0;i<PipeComponents.Count;i++)
         {
             Pipe pipeComponent = PipeComponents[i];
-            pipeComponent.Move(20);
-            if (pipeComponent.getXPos < -130f)
+            pipeComponent.Move(50);
+            if (pipeComponent.getXPos < -Pipe_Add_Pos)
             {
                 pipeComponent.Destory();
                 PipeComponents.Remove(pipeComponent);
+                i--;
             }
         }
-            
+        // Pipe Spawner
+        pipeSpawnTimer -= Time.deltaTime;
+        if(pipeSpawnTimer < 0)
+        {
+            pipeSpawnTimer += 2f;
+            SpawnedPipes += 1;
+            float SpawnSize = CalculateSizeDifficulty();
+            float minHeight = SpawnSize / 2f + SpawnHeightLimit;
+            float maxHeight = (Camera.orthographicSize * 2f) - (SpawnSize * .5f) - SpawnHeightLimit;
+            newPipe(Random.Range(minHeight,maxHeight), Pipe_Add_Pos, SpawnSize);
+        }
+
+
+    }
+    private float CalculateSizeDifficulty()
+    {
+        if (SpawnedPipes >= 100) return 20;
+        if (SpawnedPipes >= 50) return 25;
+        if (SpawnedPipes >= 40) return 30;
+        if (SpawnedPipes >= 30) return 35;
+        if (SpawnedPipes >= 20) return 40;
+        if (SpawnedPipes >= 10) return 45;
+        return 50;
     }
     private void newPipe(float gapY, float xPos, float size = 25)
     {
-        PipeComponents.Add(newRawPipe(gapY - size * .5f, xPos));
-        PipeComponents.Add(newRawPipe(Camera.orthographicSize * 2f - gapY - size * .5f, xPos, true));
+        newRawPipe(gapY - size * .5f, xPos);
+        newRawPipe(Camera.orthographicSize * 2f - gapY - size * .5f, xPos, true);
     }
-    private Pipe newRawPipe(float Height, float xPosition, bool isTop = false)
+    private void newRawPipe(float Height, float xPosition, bool isTop = false)
     {
         // Configure Pipe's Head
         Transform Head = Instantiate(AssetManager.instance.HeadPipeSprite);
@@ -74,11 +79,33 @@ public class MapRenderer : MonoBehaviour
         }            
         Body.position = new Vector2(xPosition, bodyYPos);
         SpriteRenderer BodySprite = Body.GetComponent<SpriteRenderer>();
-        BodySprite.size = new Vector2(BodySprite.size.x, Height);
+        BodySprite.size = new Vector2(BodySprite.size.x, Height - HeadHeight);
         BoxCollider2D BodyCollider = Body.GetComponent<BoxCollider2D>();
-        BodyCollider.offset = Vector2.up * Height * .5f;
-        BodyCollider.size = new Vector2(BodyCollider.size.x, Height);
+        BodyCollider.offset = Vector2.up * (Height - HeadHeight) * .5f;
+        BodyCollider.size = new Vector2(BodyCollider.size.x, Height-HeadHeight);
         // Add pipes to the Pipes list
-        return new Pipe(Head, Body);
+        PipeComponents.Add(new Pipe(Head, Body));
+    }
+    private class Pipe
+    {
+        Transform _Head;
+        Transform _Body;
+        public Pipe(Transform Head, Transform Body)
+        {
+            _Head = Head;
+            _Body = Body;
+        }
+        public void Move(float distance)
+        {
+            Vector3 newPos = Vector3.left * distance * Time.deltaTime;
+            _Head.position += newPos;
+            _Body.position += newPos;
+        }
+        public float getXPos { get => _Head.position.x; }
+        public void Destory()
+        {
+            Destroy(_Head.gameObject);
+            Destroy(_Body.gameObject);
+        }
     }
 }
