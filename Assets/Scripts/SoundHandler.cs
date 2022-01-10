e2ded9bfe4aeaaac4877dcbeb6e64b9a4a92afb9
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2022-01-09 21:09:03 -0500

    Added Sound Service

--- Assets/Scripts/SoundHandler.cs
+++ Assets/Scripts/SoundHandler.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ProtoBuf;
+[ProtoContract]
+public enum SoundOption
+{
+    [ProtoEnum]
+    Die,
+    [ProtoEnum]
+    Hit,
+    [ProtoEnum]
+    Point,
+    [ProtoEnum]
+    Swoosh,
+    [ProtoEnum]
+    Wing,
+}
 
 public class SoundHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private AudioClip Die;
+    [SerializeField]
+    private AudioClip Hit;
+    [SerializeField]
+    private AudioClip Point;
+    [SerializeField]
+    private AudioClip Swoosh;
+    [SerializeField]
+    private AudioClip Wing;
 
-    // Update is called once per frame
-    void Update()
+    private AudioSource SoundService;
+    private static SoundHandler instance;
+    private void Awake()
     {
-        
+        instance = this;
+        SoundService = GetComponent<AudioSource>();
+    }
+    public static void PlaySound(SoundOption SoundOpt)
+    {
+
+        switch(SoundOpt)
+        {
+            case SoundOption.Die:
+                instance.SoundService.PlayOneShot(instance.Die);
+                break;
+            case SoundOption.Hit:
+                instance.SoundService.PlayOneShot(instance.Hit);
+                break;
+            case SoundOption.Point:
+                instance.SoundService.PlayOneShot(instance.Point);
+                break;
+            case SoundOption.Swoosh:
+                instance.SoundService.PlayOneShot(instance.Swoosh);
+                break;
+            case SoundOption.Wing:
+                instance.SoundService.PlayOneShot(instance.Wing);
+                break;
+        }
     }
 }
