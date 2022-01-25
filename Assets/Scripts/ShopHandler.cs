a316b645415c04e2217b91f25a1619d761734520
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2022-01-24 21:23:18 -0500

    ShopUI & ShopSrc: Added Display Template

--- Assets/Scripts/ShopHandler.cs
+++ Assets/Scripts/ShopHandler.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ProtoBuf;
+using TMPro;
+using UnityEngine.UI;
+
 public class ShopHandler : MonoBehaviour
 {
-    public static Dictionary<int, ShopItem.Skins> AvailableSkins = new Dictionary<int, ShopItem.Skins>()
+    [SerializeField]
+    private Transform DisplayTemplate;
+    private static Dictionary<int, ShopItem.Skins> AvailableSkins = new Dictionary<int, ShopItem.Skins>()
     {
         { 1, new ShopItem.Skins { id = 1, name = "Default", description = "Do the default dance!!!!!", price=0 } },
         { 2, new ShopItem.Skins { id = 2, name = "Red", description = "An NFT reskin", price = 69 } },
@@ -14,6 +19,14 @@ public class ShopHandler : MonoBehaviour
     {
 
     }
+    private Transform TemplateGenerator(Transform parentobj, ShopItem.IShopItem ShopItem, Sprite ItemImg)
+    {
+        Transform Template = Instantiate(DisplayTemplate, parentobj);
+        Template.Find("Title").GetComponent<TextMeshProUGUI>().text = ShopItem.name;
+        Template.Find("Image").GetComponent<Image>().sprite = ItemImg;
+        Template.Find("Price").GetComponent<TextMeshProUGUI>().text = ShopItem.price.ToString();
+        return Template;
+    }
     private void Awake()
     {
 
@@ -21,7 +34,7 @@ public class ShopHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        RenderSkinLists();
     }
 
     // Update is called once per frame
@@ -33,8 +46,15 @@ public class ShopHandler : MonoBehaviour
 
 namespace ShopItem
 {
+    interface IShopItem
+    {
+        int id { get; set; }
+        string name { get; set; }
+        string description { get; set; }
+        int price { get; set; }
+    }
     [ProtoContract]
-    public class Skins
+    public class Skins : IShopItem
     {
         [ProtoMember(1)]
         public int id { get; set; }
